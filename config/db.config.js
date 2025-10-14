28da839c5d7a2af75ef42fead686f718d4acb3b0
aestheticNOOB <100418529+aestheticNOOB@users.noreply.github.com>
2025-10-14 11:20:39 +0100

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "localhost",
-  PORT: 27017,
-  DB: "MVP_backend"
+  HOST: process.env.DB_HOST || "localhost",
+  PORT: process.env.DB_PORT || 27017,
+  DB: process.env.DB_NAME || "MVP_backend"
 };
